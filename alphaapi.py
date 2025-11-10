17a3596e63f4273508a0d8d999d08646c604fb57
jbennett03 <141188350+jbennett03@users.noreply.github.com>
2025-11-10 15:12:21 -0600

    Update alphaapi.py

--- alphaapi.py
+++ alphaapi.py
@@ -2,8 +2,7 @@ import wolframalpha
 from dotenv import load_dotenv
 import os
 import google.generativeai as genai
-
-
+from PIL import Image
 load_dotenv()
 
 appID = os.getenv("appID")
@@ -11,39 +10,35 @@ geminiKey = os.getenv("geminiKey")
 client = wolframalpha.Client(appID)
 genai.configure(api_key=geminiKey)
 model = genai.GenerativeModel("gemini-2.5-flash")
-
+image_prompt = Image.open('capture.png')
 #problem parsing tests 
 #short answers problem
-input = input("Enter your question: ") #right now this is console input but we somehow have to change this to the drawn input
-try:
-    result = client.query(input)
-    wolframAnswer = next(result.results).text
-except Exception as e:
-    print("Error from WolframAlpha:", e)
-    wolframAnswer = "Could not receive an answer"
+def solver(question_text):
+    try:
+        result = client.query(question_text)
+        wolframAnswer = next(result.results).text
+    except Exception as e:
+        print("Error from WolframAlpha:", e)
+        wolframAnswer = "Could not receive an answer"
 
-prompt = f"""The User asked: {input}
-WolframAlpha returned the answer: {wolframAnswer}
+    prompt = [f"""The User asked: {question_text}
 
-Please explain how to solve this step-by-step in simple terms, like a tutor helping a student.
-"""
+Explain how to solve the student's problem step-by-step strictly from a math perspective. Do not include other subjects in this explanation
+""", image_prompt]
+    print("Wolframalpha: ", wolframAnswer)
 
-
-try:
-    response = model.generate_content(prompt)
-    print("\n Explanation:\n")
-    print(response.text)
-except Exception as e:
-    print("Error from Gemini:", e)
-
-
-#send the input to the wolframalpha api to solve. 
-
-
+    try:
+        response = model.generate_content(prompt)
+        print("Gemini: ", response.text)
+        return response.text
+    except Exception as e:
+        print("Error from Gemini:", e)
+        return "There was an error generating the explanation."
 
 
 
 
 
 #what do we want to acheive? 
+
 
